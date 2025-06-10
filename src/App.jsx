30f4c9ec844fe0f7370ae931e3f6cca3d431c9e3
Chisom <chisomchidike03@gmail.com>
2025-06-10 09:14:54 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { MessageSquare, RotateCcw, Search, X, Menu } from "lucide-react";
+import { MessageSquare, Play, RotateCcw, Search, X, Menu } from "lucide-react";
 
 export default function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -34,12 +34,13 @@ export default function App() {
 
       {/* Sidebar */}
       <div
-        className={`fixed inset-y-0 left-0 z-30 ${
-          sidebarOpen ? "translate-x-0" : "-translate-x-full"
-        } transform bg-white shadow-md transition-transform duration-300 ease-in-out w-48 md:w-64 md:static md:translate-x-0`}
+        className={`fixed inset-y-0 left-0 z-30 ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } transform bg-gray-150 shadow-md transition-transform duration-300 ease-in-out w-48 md:w-64 md:static md:translate-x-0`}
       >
-        <div className="flex items-center justify-between p-4 border-b">
-          <h2 className="text-lg font-bold text-gray-800">My Dashboard</h2>
+        <div className="flex items-center justify-between p-2">
+          <div className="text-gray-800 py-2 w-[25%] rounded-[10px] justify-center flex bg-gray-200">
+            <Play />
+          </div>
           {/* Remove the X button inside the sidebar for mobile view */}
           <button
             className="hidden md:block"
@@ -48,121 +49,146 @@ export default function App() {
             <X className="w-6 h-6" />
           </button>
         </div>
-        <nav className="mt-4 space-y-2 px-4">
-          <div className="menu-item relative">
-            <div
-              className={`block py-2 px-4 rounded hover:bg-gray-200 transition-opacity duration-300 ${
-                activeMenu === "Dashboard" ? "opacity-0" : "opacity-100"
-              }`}
-              onClick={() => toggleMenu("Dashboard")}
-            >
-              Dashboard
+        <nav className="mt-4 space-y-4 px-4">
+          <div className="menu-item relative cursor">
+            <div className="relative">
+              <span
+                className={`absolute top-0 left-0 w-3 h-3 bg-blue-500 rounded-full transition-opacity duration-300 ${activeMenu === "Dashboard" ? "opacity-0" : "opacity-100"
+                  }`}
+              ></span>
+              <div
+                className={`block py-2 px-2 w-[55%] rounded-[15px] text-center bg-gray-200 transition-opacity duration-300 ${activeMenu === "Dashboard" ? "opacity-0" : "opacity-100"
+                  }`}
+                onClick={() => toggleMenu("Dashboard")}
+              >
+                Data Range
+              </div>
             </div>
             {activeMenu === "Dashboard" && (
-              <div className="absolute left-full top-0 mt-2 p-1 bg-gray-100 rounded shadow w-48 transition-transform duration-300 md:w-64">
-                <div className="justify-between bg-gray-400 w-full inline-flex p-1 rounded-t mb-2">
-                  <div>Dashboard</div>
-                  <div className="inline-flex items-center">
-                    Reset
-                    <RotateCcw />
+              <div className="relative">
+                <div className="absolute left-full top-0 mt-2 p-1 bg-white rounded-[10px] shadow-lg shadow-gray-500/50 w-48 transition-transform duration-300 md:w-64">
+                  <span
+                    className="absolute top-0 left-0 w-3 h-3 bg-blue-500 rounded-full transition-opacity duration-300"
+                  ></span>
+                  <div className="justify-between bg-gray-300 w-full inline-flex p-1 rounded-b rounded-[10px] mb-2">
+                    <div>Range</div>
+                    <div className="inline-flex items-center">
+                      Reset
+                      <RotateCcw className="text-[10px] ml-2" />
+                    </div>
                   </div>
-                </div>
-                <div className="justify-between w-full inline-flex p-1 mb-2">
-                  <div>
-                    Campaign <br />
-                    <p className="text-[14px]">Spring 25 Product Launch</p>
+                  <div className="justify-between w-full inline-flex p-1 mb-2">
+                    <div className="text-black font-bold">
+                      Campaign <br />
+                      <p className="text-[14px] font-thin text-gray-400">Spring 25 Product Launch</p>
+                    </div>
+                    <div className="inline-flex items-center">
+                      <MessageSquare />
+                    </div>
                   </div>
-                  <div className="inline-flex items-center">
-                    <MessageSquare />
-                  </div>
-                </div>
-                <div className="flex justify-center mb-2">
-                  <button className="flex items-center bg-gray-300 rounded p-1">
+                  <div className="justify-center w-[90%] mb-2 flex items-center m-auto bg-gray-300 rounded-[15px] p-1">
                     Choose Resource
-                    <Search />
-                  </button>
+                    <Search className="ml-2" />
+                  </div>
                 </div>
               </div>
             )}
           </div>
+
           <div className="menu-item relative">
-            <div
-              className={`block py-2 px-4 rounded hover:bg-gray-200 transition-opacity duration-300 ${
-                activeMenu === "Profile" ? "opacity-0" : "opacity-100"
-              }`}
-              onClick={() => toggleMenu("Profile")}
-            >
-              Profile
+            <div className="relative">
+              <span
+                className={`absolute top-0 left-0 w-3 h-3 bg-blue-500 rounded-full transition-opacity duration-300 ${activeMenu === "Category" ? "opacity-0" : "opacity-100"
+                  }`}
+              ></span>
+              <div
+                className={`block py-2 px-2 w-[55%] rounded-[15px] text-center bg-gray-200 transition-opacity duration-300 ${activeMenu === "Category" ? "opacity-0" : "opacity-100"
+                  }`}
+                onClick={() => toggleMenu("Category")}
+              >
+                Category
+              </div>
             </div>
-            {activeMenu === "Profile" && (
-              <div className="absolute left-full top-0 mt-2 p-1 bg-gray-100 rounded shadow w-48 transition-transform duration-300 md:w-64">
-                <div className="justify-between bg-gray-400 w-full inline-flex p-1 rounded-t mb-2">
-                  <div>Profile</div>
+            {activeMenu === "Category" && (
+              <div className="relative">
+              <div className="absolute left-full top-0 mt-2 p-1 bg-white rounded-[10px] shadow-lg shadow-gray-500/50 w-48 transition-transform duration-300 md:w-64">
+                <span
+                  className="absolute top-0 left-0 w-3 h-3 bg-blue-500 rounded-full transition-opacity duration-300"
+                ></span>
+                <div className="justify-between bg-gray-300 w-full inline-flex p-1 rounded-b rounded-[10px] mb-2">
+                  <div>Category</div>
                   <div className="inline-flex items-center">
                     Reset
-                    <RotateCcw />
+                    <RotateCcw className="text-[10px] ml-2" />
                   </div>
                 </div>
                 <div className="justify-between w-full inline-flex p-1 mb-2">
-                  <div>
+                  <div className="text-black font-bold">
                     Campaign <br />
-                    <p className="text-[14px]">Spring 25 Product Launch</p>
+                    <p className="text-[14px] font-thin text-gray-400">Spring 25 Product Launch</p>
                   </div>
                   <div className="inline-flex items-center">
                     <MessageSquare />
                   </div>
                 </div>
-                <div className="flex justify-center mb-2">
-                  <button className="flex items-center bg-gray-300 rounded p-1">
-                    Choose Resource
-                    <Search />
-                  </button>
+                <div className="justify-center w-[90%] mb-2 flex items-center m-auto bg-gray-300 rounded-[15px] p-1">
+                  Choose Resource
+                  <Search className="ml-2" />
                 </div>
               </div>
+            </div>
             )}
           </div>
+
           <div className="menu-item relative">
-            <div
-              className={`block py-2 px-4 rounded hover:bg-gray-200 transition-opacity duration-300 ${
-                activeMenu === "Settings" ? "opacity-0" : "opacity-100"
-              }`}
-              onClick={() => toggleMenu("Settings")}
-            >
-              Settings
-            </div>
-            {activeMenu === "Settings" && (
-              <div className="absolute left-full top-0 mt-2 p-1 bg-gray-100 rounded shadow w-48 transition-transform duration-300 md:w-64">
-                <div className="justify-between bg-gray-400 w-full inline-flex p-1 rounded-t mb-2">
-                  <div>Settings</div>
-                  <div className="inline-flex items-center">
-                    Reset
-                    <RotateCcw />
-                  </div>
-                </div>
-                <div className="justify-between w-full inline-flex p-1 mb-2">
-                  <div>
-                    Campaign <br />
-                    <p className="text-[14px]">Spring 25 Product Launch</p>
-                  </div>
-                  <div className="inline-flex items-center">
-                    <MessageSquare />
-                  </div>
-                </div>
-                <div className="flex justify-center mb-2">
-                  <button className="flex items-center bg-gray-300 rounded p-1">
-                    Choose Resource
-                    <Search />
-                  </button>
-                </div>
+            <div className="relative">
+              <div
+                className={`block py-2 px-2 w-[55%] rounded-[15px] text-center bg-gray-200 transition-opacity duration-300 ${activeMenu === "Origin" ? "opacity-0" : "opacity-100"
+                  }`}
+                onClick={() => toggleMenu("Origin")}
+              >
+                Origin Level
               </div>
+            </div>
+            {activeMenu === "Origin" && (
+             <div className="relative">
+             <div className="absolute left-full top-0 mt-2 p-1 bg-white rounded-[10px] shadow-lg shadow-gray-500/50 w-48 transition-transform duration-300 md:w-64">
+               <span
+                 className="absolute top-0 left-0 w-3 h-3 bg-blue-500 rounded-full transition-opacity duration-300"
+               ></span>
+               <div className="justify-between bg-gray-300 w-full inline-flex p-1 rounded-b rounded-[10px] mb-2">
+                 <div>Origin</div>
+                 <div className="inline-flex items-center">
+                   Reset
+                   <RotateCcw className="text-[10px] ml-2" />
+                 </div>
+               </div>
+               <div className="justify-between w-full inline-flex p-1 mb-2">
+                 <div className="text-black font-bold">
+                   Campaign <br />
+                   <p className="text-[14px] font-thin text-gray-400">Spring 25 Product Launch</p>
+                 </div>
+                 <div className="inline-flex items-center">
+                   <MessageSquare />
+                 </div>
+               </div>
+               <div className="justify-center w-[90%] mb-2 flex items-center m-auto bg-gray-300 rounded-[15px] p-1">
+                 Choose Resource
+                 <Search className="ml-2" />
+               </div>
+             </div>
+           </div>
             )}
           </div>
-          <a
-            href="#"
-            className="block py-2 px-4 rounded hover:bg-red-100 text-red-500 hover:text-red-700"
-          >
-            Logout
-          </a>
+
+
+
+
+
+
+          <div className="text-gray-800 py-2 w-[25%] rounded-[10px] justify-center flex bg-gray-200">
+            <RotateCcw />
+          </div>
         </nav>
       </div>
     </div>
